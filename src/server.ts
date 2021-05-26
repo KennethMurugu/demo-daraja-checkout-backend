44e6e82eef336fcfa3ff30c4ccd79373855b444f
Kenneth Murugu <kennethmurugu@gmail.com>
2021-05-26 11:17:02 +0300

    dependecny udpates
    Signed-off-by: Kenneth Murugu <kennethmurugu@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import { getOAuthToken, lnm, lnmcallback } from './api'
 
@@ -8,29 +7,25 @@ dotenv.config()
 
 const serverIP: any = process.env.SERVER_IP
 if (!serverIP) {
-  throw new Error(`Invalid value SERVER_IP: ${serverIP}`)
+	throw new Error(`Invalid value SERVER_IP: ${serverIP}`)
 }
 
 let app = express()
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(function (req, response, next) {
-  console.log(`[Request] path=${req.path}, body=${JSON.stringify(req.body)}`)
-  next()
+	console.log(`[Request] path=${req.path}, body=${JSON.stringify(req.body)}`)
+	next()
 })
 app
-  .get('/test', function (req, res) {
-    res.send('MAIN TEST URL')
-  })
-  .get('/oauthtoken', getOAuthToken)
-  .post('/lnm', lnm)
-  .get('/lnmcallback', lnmcallback)
+	.get('/test', function (req, res) {
+		res.send('MAIN TEST URL')
+	})
+	.get('/oauthtoken', getOAuthToken)
+	.post('/lnm', lnm)
+	.get('/lnmcallback', lnmcallback)
 
 app.listen(8081, serverIP, () => {
-  console.info(`Backend Server listening on ${serverIP}:8081`)
+	console.info(`Backend Server listening on ${serverIP}:8081`)
 })
